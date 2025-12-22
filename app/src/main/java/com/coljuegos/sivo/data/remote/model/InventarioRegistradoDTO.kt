991bd6a42869ef05c301970ce458d336520ae833
Cristian Osia <osia.developer@gmail.com>
2025-12-22 04:37:35 -0500

    Se agregan las tablas

--- app/src/main/java/com/coljuegos/sivo/data/remote/model/InventarioRegistradoDTO.kt
+++ app/src/main/java/com/coljuegos/sivo/data/remote/model/InventarioRegistradoDTO.kt
@@ -15,8 +15,36 @@ data class InventarioRegistradoDTO(
     @SerializedName("estado")
     val estado: String,
 
+    // Campos de verificación de código de apuesta
+    @SerializedName("codigoApuestaDiferente")
+    val codigoApuestaDiferente: Boolean,
+
+    @SerializedName("codigoApuestaDiferenteValor")
+    val codigoApuestaDiferenteValor: String?,
+
+    // Campos de verificación de serial
+    @SerializedName("serialVerificado")
+    val serialVerificado: Boolean,
+
+    @SerializedName("serialDiferente")
+    val serialDiferente: String?,
+
+    // Campos de verificación de características
+    @SerializedName("descripcionJuego")
+    val descripcionJuego: Boolean,
+
+    @SerializedName("planPremios")
+    val planPremios: Boolean,
+
+    @SerializedName("valorPremios")
+    val valorPremios: Boolean,
+
     @SerializedName("valorCredito")
     val valorCredito: String?,
+
+    // Campo de verificación de contadores
+    @SerializedName("contadoresVerificado")
+    val contadoresVerificado: Boolean,
 
     @SerializedName("coinInMet")
     val coinInMet: String?,
